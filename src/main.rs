526670118ed4a7bc0217cb9d56cec67edde58441
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-17 23:55:49 +0200

    stage 3 pings

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,17 @@ fn main() -> anyhow::Result<()> {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => loop {
-                let mut buf = [0; 1024];
-                stream.read(&mut buf)?;
-                stream.write_all("+PONG\r\n".as_bytes())?;
-            },
-            Err(e) => {
-                println!("error: {}", e);
+    loop {
+        match listener.accept() {
+            Ok((mut stream, addr)) => {
+                println!("new client with address: {addr} connected");
+                thread::spawn(move || {
+                    let mut buf = [0; 1024];
+                    stream.read(&mut buf).unwrap();
+                    stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+                });
             }
+            Err(e) => println!("Couldn't get client: {:?}", e),
         }
     }
-    Ok(())
 }
