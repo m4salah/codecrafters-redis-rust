dfafb3a44a7294415e028c00fa056f03ef12ce2b
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-17 22:24:11 +0200

    stage 2 ping pong

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -9,12 +12,22 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut buf = [0; 1024];
+                stream.read(&mut buf)?;
+                let buf_string = String::from_utf8_lossy(&buf);
+                let mut splited = buf_string.split("\r\n");
+                if splited.next().unwrap() == "*1"
+                    && splited.next().unwrap() == "$4"
+                    && splited.next().unwrap().to_lowercase() == "ping"
+                {
+                    stream.write_all("+PONG\r\n".as_bytes())?;
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
