cfcd8bdd1ef9f19b76571e69933a6b83147285a4
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-20 22:20:16 +0200

    handle expiry in set command

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,20 @@ use std::{
     net::{TcpListener, TcpStream},
     sync::{Arc, Mutex},
     thread,
+    time::{Duration, SystemTime},
 };
 
 struct Database {
-    db: HashMap<String, String>,
+    db: HashMap<String, DatabaseValue>,
+}
+
+struct DatabaseValue {
+    time: Option<Time>,
+    value: String,
+}
+
+struct Time {
+    expires_at: SystemTime,
 }
 
 impl Database {
@@ -17,11 +27,33 @@ impl Database {
     }
 
     fn get(&self, key: &str) -> Option<&String> {
-        self.db.get(key)
+        if let Some(value) = self.db.get(key) {
+            if let Some(time) = &value.time {
+                let now = SystemTime::now();
+                if time.expires_at > now {
+                    return Some(&value.value);
+                }
+            }
+        }
+        None
     }
 
-    fn set(&mut self, key: &str, value: &str) -> Option<String> {
-        self.db.insert(key.to_owned(), value.to_owned())
+    fn set(&mut self, key: &str, value: &str, expiry_ms: Option<u64>) -> Option<DatabaseValue> {
+        if let Some(ms) = expiry_ms {
+            let now = SystemTime::now();
+            let expiry_duration = Duration::from_millis(ms);
+            let expires_at = now + expiry_duration;
+            let value = DatabaseValue {
+                time: Some(Time { expires_at }),
+                value: value.to_string(),
+            };
+            return self.db.insert(key.to_owned(), value);
+        }
+        let value = DatabaseValue {
+            time: None,
+            value: value.to_string(),
+        };
+        self.db.insert(key.to_owned(), value)
     }
 }
 
@@ -66,24 +98,36 @@ fn handle_connection(mut stream: TcpStream, state: Arc<Mutex<Database>>) -> anyh
             }
         }
         if !commands.is_empty() {
-            if commands[0].to_lowercase() == "echo" {
-                stream.write_all(format!("+{}\r\n", commands[1]).as_bytes())?;
-                continue;
-            }
-            if commands[0].to_lowercase() == "set" {
-                {
-                    state.lock().unwrap().set(&commands[1], &commands[2]);
+            match commands[0].to_lowercase().as_str() {
+                "echo" => {
+                    stream.write_all(format!("+{}\r\n", commands[1]).as_bytes())?;
+                    continue;
                 }
-                stream.write_all(format!("+OK\r\n").as_bytes())?;
-                continue;
-            }
-            if commands[0].to_lowercase() == "get" {
-                if let Some(v) = state.lock().unwrap().get(&commands[1]) {
-                    stream.write_all(format!("+{v}\r\n").as_bytes())?;
-                } else {
-                    stream.write_all(format!("$-1\r\n").as_bytes())?;
+                "set" => {
+                    if let Some(px_position) =
+                        commands.iter().position(|v| v.to_lowercase() == "px")
+                    {
+                        let ms: u64 = commands[px_position + 1].parse().unwrap();
+                        state
+                            .lock()
+                            .unwrap()
+                            .set(&commands[1], &commands[2], Some(ms));
+                        stream.write_all(format!("+OK\r\n").as_bytes())?;
+                    } else {
+                        state.lock().unwrap().set(&commands[1], &commands[2], None);
+                        stream.write_all(format!("+OK\r\n").as_bytes())?;
+                    }
+                    continue;
                 }
-                continue;
+                "get" => {
+                    if let Some(v) = state.lock().unwrap().get(&commands[1]) {
+                        stream.write_all(format!("+{v}\r\n").as_bytes())?;
+                    } else {
+                        stream.write_all(format!("$-1\r\n").as_bytes())?;
+                    }
+                    continue;
+                }
+                _ => {}
             }
         }
         eprintln!("{:?}", commands);
