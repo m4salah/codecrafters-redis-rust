5ef14956cf632163a21b80936443cff3faeace8d
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-20 21:03:41 +0200

    handle get and set command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 // Uncomment this block to pass the first stage
 use std::{
+    collections::HashMap,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
     thread,
 };
+
+struct Database {
+    db: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Database {
+        Database { db: HashMap::new() }
+    }
+
+    fn get(&self, key: &str) -> Option<&String> {
+        self.db.get(key)
+    }
+
+    fn set(&mut self, key: &str, value: &str) -> Option<String> {
+        self.db.insert(key.to_owned(), value.to_owned())
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,8 +33,9 @@ fn main() -> anyhow::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                let db = Database::new();
                 thread::spawn(move || {
-                    if let Err(e) = handle_connection(stream) {
+                    if let Err(e) = handle_connection(stream, db) {
                         eprintln!("ERROR: while handling the connection: {e}");
                     }
                 });
@@ -28,27 +48,41 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
+fn handle_connection(mut stream: TcpStream, mut db: Database) -> anyhow::Result<()> {
     let mut buf = [0; 1024];
     loop {
         let n = stream.read(&mut buf)?;
         let buf_str = String::from_utf8_lossy(&buf[..n]);
         eprintln!("{}", buf_str);
         let splitted = buf_str.split("\r\n");
-        let mut v = Vec::new();
+
+        let mut commands = Vec::new();
         for command in splitted {
             if !command.starts_with("*") && !command.starts_with("$") && command != "" {
                 eprintln!("{:?}", command);
-                v.push(command);
+                commands.push(command);
             }
         }
-        if !v.is_empty() {
-            if v[0].to_lowercase() == "echo" {
-                stream.write_all(format!("+{}\r\n", v[1]).as_bytes())?;
+        if !commands.is_empty() {
+            if commands[0].to_lowercase() == "echo" {
+                stream.write_all(format!("+{}\r\n", commands[1]).as_bytes())?;
+                continue;
+            }
+            if commands[0].to_lowercase() == "set" {
+                db.set(&commands[1], &commands[2]);
+                stream.write_all(format!("+OK\r\n").as_bytes())?;
+                continue;
+            }
+            if commands[0].to_lowercase() == "get" {
+                if let Some(v) = db.get(&commands[1]) {
+                    stream.write_all(format!("+{v}\r\n").as_bytes())?;
+                } else {
+                    stream.write_all(format!("$-1\r\n").as_bytes())?;
+                }
                 continue;
             }
         }
-        eprintln!("{:?}", v);
+        eprintln!("{:?}", commands);
         stream.write_all("+PONG\r\n".as_bytes())?;
     }
 }
